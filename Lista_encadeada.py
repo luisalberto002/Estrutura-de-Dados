624e9e776feabea11571281ab826e9701ff29657
Luis Alberto <103611641+luisalberto002@users.noreply.github.com>
2023-03-30 00:13:05 -0300

    Atulização

--- Lista_encadeada.py
+++ Lista_encadeada.py
@@ -1,30 +1,101 @@
 from No import No
 
+
 class Lista_encadeada:
     def __init__(self):
         self.inicio = None
         self.tamanho = 0
 
-    def inserir(self, valor):
+    def append(self, valor):
         no = No(valor)
         if self.tamanho == 0:
             self.inicio = no
-            self.inicio.proximo = None
         else:
             aux = self.inicio
             while aux.proximo is not None:
                 aux = aux.proximo
             aux.proximo = no
         self.tamanho += 1
-            
-    def mostrar (self):
+
+    def mostrar(self):
+        if self.tamanho > 0:
+            aux = self.inicio
+            while aux is not None:
+                print(aux.valor, end=" ")
+                aux = aux.proximo
+            print()
+        else:
+            print("Lista vazia")
+
+    def insert(self, index, valor):
+        if index <= 0:
+            no = No(valor)
+            no.proximo = self.inicio
+            self.inicio = no
+            self.tamanho += 1
+        elif index >= self.tamanho:
+            self.append(valor)
+        else:
+            aux = self.inicio
+            for i in range(index - 1):
+                aux = aux.proximo
+            no = No(valor)
+            no.proximo = aux.proximo
+            aux.proximo = no
+            self.tamanho += 1
+
+    def remove(self, valor):
         aux = self.inicio
-        while aux is not None:
-            print(aux.valor)
-            aux = aux.proximo
+        while aux.proximo is not None:
+            if aux.proximo.valor == valor:
+                aux.proximo = aux.proximo.proximo
+                self.tamanho -= 1
+                break
+            else:
+                aux = aux.proximo
+
+    def pop(self):
+        if self.tamanho > 0:
+            aux = self.inicio
+            for i in range(self.tamanho - 2):
+                aux = aux.proximo
+            aux.proximo = None
+            self.tamanho -= 1
+        else:
+            print("Não é possível remover um elemento de uma lista vazia")
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        if self.tamanho > 0:
+            r = ""
+            aux = self.inicio
+            while aux is not None:
+                r += str(aux.valor) + " "
+                aux = aux.proximo
+            return r
+        else:
+            return "lista vazia"
+
+    def __getitem__(self, index):
+        aux = self.inicio
+        if index < 0 or index >= self.size:
+            return "list index out of range"
+        else:
+            for i in range(index):
+                aux = aux.proximo
+            return aux.valor
+
 
 lista = Lista_encadeada()
-lista.inserir(23)
-lista.inserir(42)
-lista.mostrar()
+
+lista.append(23)
+lista.append(42)
+lista.append(25)
+lista.insert(1, 10)
+# lista.pop()
+# lista.remove(10)
+print(lista)
+print(lista[-4])
 print(f'tamanho: {lista.tamanho}')
